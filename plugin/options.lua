30f992ac974c550acea780ed52172e068a3665e3
James Tan <jamestansx@gmail.com>
2025-02-19 23:51:25 +0800

    set options

---
+++ plugin/options.lua
@@ -0,0 +1,82 @@
+vim.o.confirm = true
+vim.o.exrc = true
+vim.o.mouse = "a"
+vim.o.mousemodel = "extend"
+vim.o.selection = "old" -- don't select past line
+vim.o.shada = "'100,<50,s10,:100,/100,@100,h,r/tmp"
+vim.o.termguicolors = true
+vim.o.undofile = true
+vim.o.virtualedit = "block"
+vim.opt.isfname:append("@-@")
+
+-- editing
+vim.o.jumpoptions = "clean,stack,view"
+vim.o.scrolloff = 5
+vim.o.sidescroll = 3
+vim.o.sidescrolloff = 3
+vim.opt.matchpairs:append("<:>")
+vim.opt.diffopt:append("algorithm:histogram,indent-heuristic")
+
+-- completion
+vim.o.completeopt = "menuone,noselect,fuzzy"
+vim.o.pumheight = 5
+vim.o.wildcharm = (""):byte()
+vim.o.wildmode = "longest:full,full"
+vim.opt.wildignore:append("*/__pycache__/*,*/node_modules/*")
+
+-- highlight 'number' column instead of indentation
+vim.o.cursorline = true
+vim.o.cursorlineopt = "number"
+vim.o.numberwidth = 1
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- consistent window splitting
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+-- less noise
+vim.o.laststatus = 3
+vim.o.showmode = false
+vim.o.shortmess = "aoOstTAIcCF"
+vim.o.showcmd = false
+vim.o.signcolumn = "yes:1"
+
+-- spaces between us
+vim.o.expandtab = true
+vim.o.shiftwidth = 0
+vim.o.softtabstop = -1
+vim.o.shiftround = true
+
+-- improve search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.inccommand = "split"
+
+-- wrap option
+vim.o.wrap = false
+vim.o.breakindent = true
+vim.o.linebreak = true
+
+vim.o.list = true
+vim.o.listchars = table.concat({
+    "extends:🠞",  -- U+1F81E
+    "nbsp:⦸",     -- U+29B8
+    "precedes:🠜", -- U+1F81C
+    "tab:  »",    -- U+00BB
+    "trail:·",    -- U+00B7
+}, ",")
+
+if vim.fn.executable("rg") == 1 then
+    vim.o.grepprg = "rg --vimgrep --smart-case -uu"
+    vim.o.grepformat = "%f:%l:%c:%m,%f:%l:%m"
+end
+
+-- ftplugin may include 'o' option
+vim.api.nvim_create_autocmd("FileType", { command = "setlocal formatoptions-=o" })
+
+vim.diagnostic.config({
+    severity_sort = true,
+    jump = { float = true },
+    sign = { severity = { min = 'WARN', max = 'ERROR' } },
+})
